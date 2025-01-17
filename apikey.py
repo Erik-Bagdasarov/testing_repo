f80b611f03deb89875f0a2b1cdcc68e6050ec9da
Erik-Bagdasarov <124051964+Erik-Bagdasarov@users.noreply.github.com>
2025-01-17 16:07:24 +0500

    deleted unnecessary files and directories. Fixed payme.ipynb

--- apikey.py
+++
@@ -1,1 +0,0 @@
-API_TOKEN = '364faf099086a4c87dd1a716cea1d126'
--- main.py
+++
@@ -1,5 +0,0 @@
-file = open('test.txt', 'a')
-file.write("hello world")
-lines = ["Line 1\n", "Line 2\n", "Line 3\n"]
-file.writelines(lines)
-file.close()
